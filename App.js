585903e55c4111edc4263c0edc11090c70b333dd
Meilee <meilee@github.com>
2018-03-06 16:44:53 -0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,203 @@
+import React from 'react';
+import { ListView, StyleSheet, View } from 'react-native';
+import { Body, Title, Right, Container, Header, Content, Button, Icon, List, ListItem, Text } from 'native-base';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dates: []
+    }
+  }
+
+  // Retrieve the list of ideas from Airtable
+  getIdeas() {
+    // Airtable API endpoint, replace with your own
+    let airtableUrl = "https://api.airtable.com/v0/apphz4CdADixOacYu/dates/"; // Update the base ID
+
+    // Needed for Airtable authorization, replace with your own API key
+    let requestOptions = {
+      headers: new Headers({
+        'Authorization': 'Bearer key5rXxddRt2EOl5H'
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.setState({
+        dates: json.records
+      });
+    });
+  }
+
+  // Runs when the application loads (i.e. the "App" component "mounts")
+  componentDidMount() {
+    this.getIdeas(); // refresh the list when we're done
+  }
+
+  // Upvote an idea
+  upvoteIdea(data, secId, rowId, rowMap) {
+    // Slide the row back into place
+    rowMap[`${secId}${rowId}`].props.closeRow();
+
+    // Airtable API endpoint
+    let airtableUrl = "https://api.airtable.com/v0/apphz4CdADixOacYu/dates/" + data.id;
+
+    // Needed for Airtable authorization
+    let requestOptions = {
+      method: 'PATCH',
+      headers: new Headers({
+        'Authorization': 'Bearer key5rXxddRt2EOl5H', // replace with your own API key
+        'Content-type': 'application/json'
+      }),
+      body: JSON.stringify({
+        fields: {
+          votes: data.fields.votes + 1
+        }
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.getIdeas(); // refresh the list when we're done
+    });
+  }
+
+  // Downvote an idea
+  downvoteIdea(data, secId, rowId, rowMap) {
+    // Slide the row back into place
+    rowMap[`${secId}${rowId}`].props.closeRow();
+
+    // Airtable API endpoint
+    let airtableUrl = "https://api.airtable.com/v0/apphz4CdADixOacYu/dates/" + data.id;
+
+    // Needed for Airtable authorization
+    let requestOptions = {
+      method: 'PATCH',
+      headers: new Headers({
+        'Authorization': 'Bearer key5rXxddRt2EOl5H', // replace with your own API key
+        'Content-type': 'application/json'
+      }),
+      body: JSON.stringify({
+        fields: {
+          votes: data.fields.votes - 1
+        }
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.getIdeas(); // refresh the list when we're done
+    });
+  }
+
+  // Ignore an idea: The idea doesn't disappear from the database.
+  ignoreIdea(data, secId, rowId, rowMap) {
+    // Slide the row back into place
+    rowMap[`${secId}${rowId}`].props.closeRow();
+
+    // Create a new array that has the idea removed
+    let newIdeasData = this.state.dates.slice();
+    newIdeasData.splice(rowId, 1);
+
+    // Set state
+    this.setState({
+      dates: newIdeasData
+    });
+  }
+
+  // Delete an idea: The idea disppears from the database.
+  deleteIdea(data, secId, rowId, rowMap) {
+    // Slide the row back into place
+    rowMap[`${secId}${rowId}`].props.closeRow();
+
+    // Create a new array that has the idea removed
+    let newIdeasData = this.state.dates.slice();
+    newIdeasData.splice(rowId, 1);
+
+    // Airtable API endpoint
+    let airtableUrl = "https://api.airtable.com/v0/apphz4CdADixOacYu/dates/" + data.id;
+
+    // Needed for Airtable authorization
+    let requestOptions = {
+      method: 'DELETE',
+      headers: new Headers({
+        'Authorization': 'Bearer key5rXxddRt2EOl5H', // replace with your own API key
+        'Content-type': 'application/json'
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.getIdeas(); // refresh the list when we're done
+    });
+  }
+
+  // The UI for each row of data
+  renderRow(data) {
+    return (
+      <ListItem style={{ paddingLeft: 20, paddingRight: 20 }}>
+        <Body>
+          <Text>{data.fields.description}</Text>
+        </Body>
+        <Right>
+          <Text note>{data.fields.votes} votes</Text>
+        </Right>
+      </ListItem>
+    )
+  }
+
+  // The UI for what appears when you swipe right
+  renderSwipeRight(data, secId, rowId, rowMap) {
+    return (
+      <Button full success onPress={() => this.upvoteIdea(data, secId, rowId, rowMap)}>
+        <Icon active name="thumbs-up" />
+      </Button>
+    )
+  }
+
+  // The UI for what appears when you swipe left
+  renderSwipeLeft(data, secId, rowId, rowMap) {
+    return (
+      <Button full danger onPress={() => this.deleteIdea(data, secId, rowId, rowMap)}>
+        <Icon active name="thumbs-down" />
+      </Button>
+    )
+  }
+
+  render() {
+    let rows = this.ds.cloneWithRows(this.state.dates);
+    return (
+      <Container>
+        <Header>
+          <Body>
+            <Title>Date Picker</Title>
+          </Body>
+        </Header>
+        <Content>
+          <List
+            dataSource={rows}
+            renderRow={(data) => this.renderRow(data)}
+            renderLeftHiddenRow={(data, secId, rowId, rowMap) => this.renderSwipeRight(data, secId, rowId, rowMap)}
+            renderRightHiddenRow={(data, secId, rowId, rowMap) => this.renderSwipeLeft(data, secId, rowId, rowMap)}
+            leftOpenValue={75}
+            rightOpenValue={-75}
+          />
+        </Content>
+      </Container>
+    );
+  }
+}
